3ff7a06f1e99a86cb1d0fd3b2d648afb21cedfdf
Nitin Singh <vortechcorp@gmail.com>
2023-03-29 17:48:13 +0530

    completed the task

---
+++ index.js
@@ -0,0 +1,13 @@
+const calculateAnswer = (email) =>
+
+{
+
+const [localPart, domain] = email.split("@");
+
+const [hostname, ...countryCodes] = domain.split(".");
+
+return `${localPart.length}${hostname.length}${countryCodes.reduce( (a,cc) => a + cc.length, "") }`
+
+}
+
+console.log(calculateAnswer('nitin@gmail.com'));
